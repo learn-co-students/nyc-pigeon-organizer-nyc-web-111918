652c498831464363a6b06112fda4bfd8e8403516
satyasinha94 <satyasinha94@gmail.com>
2018-11-11 18:23:25 -0500

    Done

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |data_type, characteristics|
+    characteristics.each do |info, pigeons|
+      pigeons.each do |pigeon|
+        new_hash[pigeon] ||= {}
+        new_hash[pigeon][data_type] ||= []
+        new_hash[pigeon][data_type] << info.to_s
+      end
+    end
+  end
+  new_hash
 end
