8a1240fa05758e03b75a53ecbf5af4d86ffdd73d
Simon Lee <simonlee0306@gmail.com>
2018-11-11 21:42:07 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+
+  data.each do |category, attributes| #color, gender, lives
+    attributes.each do |attribute, data_array| #purple white, male female, "Subway" "Library"
+      data_array.each do |name|
+        if pigeon_list.has_key?(name) == false
+          pigeon_list[name] = {:color => [], :gender => [], :lives => []}
+          pigeon_list[name][category] << attribute.to_s
+#          binding.pry
+        else
+          pigeon_list[name][category] << attribute.to_s
+        end
+      end
+    end
+  end
+  binding.pry
+  pigeon_list
 end
